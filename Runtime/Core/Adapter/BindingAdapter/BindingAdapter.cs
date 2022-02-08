a0b49c39d086f03862b377a4c006ba41b618ed3c
rehawk_ <25656754+rehavvk@users.noreply.github.com>
2022-02-08 10:50:46 +0100

    Added basic binding adapter

---
+++ Runtime/Core/Adapter/BindingAdapter/BindingAdapter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace Rehawk.UIFramework.Adapter
+{
+    public abstract class BindingAdapter : Adapter<ContextControl>
+    {
+        [SerializeField] private string binding;
+        
+        private bool hadContext;
+
+        public string Binding
+        {
+            get { return binding; }
+        }
+        
+        protected override void OnRefresh()
+        {
+            base.OnRefresh();
+
+            if (Control.HasContext)
+            {
+                if (!hadContext)
+                {
+                    OnGotContext();
+                }
+            }
+            else
+            {
+                if (hadContext)
+                {
+                    OnLostContext();
+                }
+            }
+
+            hadContext = Control.HasContext;
+        }
+
+        protected abstract void OnGotContext();
+        protected abstract void OnLostContext();
+        
+        protected T GetValue<T>(string path, T fallback = default)
+        {
+            object context = Control.GetContext();
+            Type contextType = context.GetType();
+
+            object value = null;
+            
+            FieldInfo fieldInfo = contextType.GetField(path, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            if (fieldInfo != null)
+            {
+                value = fieldInfo.GetValue(context);
+            }
+            else
+            {
+                PropertyInfo propertyInfo = contextType.GetProperty(path, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                if (propertyInfo != null)
+                {
+                    value = propertyInfo.GetValue(context);
+                }
+            }
+
+            return value != null ? (T) value : fallback;
+        }
+    }
+}
---
+++ Runtime/Core/Adapter/BindingAdapter/TextBindingAdapter.cs
@@ -0,0 +1,82 @@
+﻿using System.Text.RegularExpressions;
+using RSG;
+using TMPro;
+using UnityEngine;
+
+namespace Rehawk.UIFramework.Adapter
+{
+    public class TextBindingAdapter : BindingAdapter
+    {
+        [SerializeField] private TextMeshProUGUI label;
+        
+        protected override void OnGotContext()
+        {
+            label.text = string.Empty;
+            
+            if (Binding.Contains("{"))
+            {
+                ProcessFormat(Binding)
+                    .Then(result =>
+                    {
+                        label.text = result;
+                    })
+                    .Catch(Debug.LogError);
+            }
+            else
+            {
+                label.text = GetValue<object>(Binding).ToString();
+            }
+        }
+
+        protected override void OnLostContext()
+        {
+            label.text = string.Empty;
+        }
+        
+        private IPromise<string> ProcessFormat(string format)
+        {
+            var promise = new Promise<string>();
+            
+            Match match = Regex.Match(format, "{(.[^}]*)}");
+
+            if (match.Success)
+            {
+                ProcessFormatRecursive(format, match)
+                    .Then(promise.Resolve)
+                    .Catch(promise.Reject);
+            }
+            else
+            {
+                promise.Resolve(string.Empty);
+            }
+
+            return promise;
+        }
+        
+        private IPromise<string> ProcessFormatRecursive(string format, Match match)
+        {
+            var promise = new Promise<string>();
+            
+            string dataKey = match.Groups[1].ToString();
+
+            object value = GetValue<object>(dataKey);
+            string replacement = value != null ? value.ToString() : "";
+                    
+            format = format.Replace(match.Value, replacement);
+            match = match.NextMatch();
+
+            if (match.Success)
+            {
+                ProcessFormatRecursive(format, match)
+                    .Then(promise.Resolve)
+                    .Catch(promise.Reject);
+            }
+            else
+            {
+                promise.Resolve(format);
+            }
+            
+            return promise;
+        }
+    }
+}
